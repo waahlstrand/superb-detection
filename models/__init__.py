63f9fa336c05afc6b32ef8461b4420cfb2d5d4e9
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2024-02-18 12:17:23 +0100

    Removed vertebra-detr

--- models/__init__.py
+++ models/__init__.py
@@ -1,29 +1,17 @@
-from .models import VertebraeDetector, VertebraViDT
 from .criterion import RLELoss
+from .spine import *
 
-def build_model(args):
+def build_model(args, class_weights: List[float] = None):
 
-    if args.model == "vertebra-detector":
+    if args.model == "spine-dino":
 
-        model = VertebraeDetector(
-            n_vertebrae=13,
-            n_keypoints=6,
-            n_dims=2,
-            backbone=models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1),
-            criterion=RLELoss,
-            lr=args.lr,
-            lr_backbone=args.lr_backbone,
-            weight_decay=args.weight_decay,
-        )
-
-    elif args.model == "vertebra-vidt":
-
-        model = VertebraViDT(
+        model = SpineDINO(
                  args, 
                  n_classes= args.n_classes, 
-                 n_keypoints = 6, 
-                 n_dim = 2, 
+                 n_keypoints = args.n_keypoints, 
+                 n_dim = args.n_dims, 
                  n_channels = 3,
+                 class_weights = class_weights
         )
 
     else:
